0bd44991c48b631fc78015372a0450b4fe61aabe
Andreas Hrubak <ad132454@uucp.hu>
2021-12-16 11:14:11 +0100

    initial commit

---
+++ dnsmus.pl
@@ -0,0 +1,144 @@
+
+use Net::DNS;
+use Net::DNS::Packet;
+use Net::DNS::RR;
+use Data::Dumper;
+use IO::Socket::INET;
+
+$, = "\t";
+$\ = "\n";
+$timeout = 2;
+
+($ListenPort, @Resolvers) = @ARGV;
+
+sub getresolvers
+{
+	return @Resolvers;
+}
+
+sub serialize
+{
+	my $oref = shift;
+	my %hash = %$oref;
+	delete $hash{'rdata'};
+	return join " ", map {"$_=".$hash{$_}} sort keys %hash;
+}
+
+
+my @upstream;
+
+for my $resolver_addr (getresolvers)
+{
+	my $res = Net::DNS::Resolver->new;
+	$res->udp_timeout($timeout);
+	$res->tcp_timeout($timeout);
+	$res->retry(0);
+	$res->retrans(0);
+	$res->dnsrch(0);
+	$res->nameservers($resolver_addr);
+	push @upstream, {'res' => $res, };
+}
+
+
+my $socket = IO::Socket::INET->new(LocalAddr => '0.0.0.0', LocalPort => $ListenPort, Proto => 'udp') or die $!;
+
+QUERY:
+while(1)
+{
+	$socket->recv($incoming_packet_data, 1024);
+	my $peer_address = $socket->peerhost();
+	my $peer_port = $socket->peerport();
+	
+	my $incoming_packet = new Net::DNS::Packet(\$incoming_packet_data) or die $!;
+	print STDERR ">>> $peer_address:$peer_port", serialize $incoming_packet->{'header'};
+	print STDERR "", serialize $_ for @{$incoming_packet->{'question'}};
+	#warn Dumper $incoming_packet;
+	
+	for $upstream (@upstream)
+	{
+		$upstream->{'query'} = $upstream->{'res'}->bgsend($incoming_packet);
+		$upstream->{'started'} = time;
+	}
+	
+	RESPONSE:
+	while(1)
+	{
+		my $busy = 0;
+		UPSTEAM:
+		for $upstream (@upstream)
+		{
+			my $res = $upstream->{'res'};
+			my $query_handle = $upstream->{'query'};
+			next UPSTEAM if not defined $query_handle;
+			#print Dumper $query_handle;
+			
+			if($res->bgisready($query_handle))
+			{
+				my $response = $res->bgread($query_handle);
+				#$response->print;
+				$upstream->{'lastresponse'} = $response;
+				my $header = $response->{'header'};
+				
+				#warn Dumper $response->answer;
+				print STDERR "<<< $response->{'answerfrom'}", serialize $header;
+				print STDERR "", serialize $_ for $response->answer;
+				
+				close $upstream->{'query'};
+				undef $upstream->{'query'};
+				
+				if($header->{'rcode'} eq 'NOERROR'
+				   and $header->{'ancount'} > 0
+				  )
+				{
+					last RESPONSE;
+				}
+			}
+			else
+			{
+				#$res->print;
+				if($upstream->{'started'} + $timeout < time)
+				{
+					$upstream->{'query'} = undef;
+				}
+				else
+				{
+					$busy++;
+				}
+			}
+		}
+		last RESPONSE if $busy == 0;
+		sleep 0.1;
+	}
+	
+	my $replied = 0;
+	
+	for $upstream (@upstream)
+	{
+		my $response_packet = $upstream->{'lastresponse'};
+		if(defined $response_packet)
+		{
+			my $rr = Net::DNS::RR->new("upstream-resolver-address. 0 CH TXT \"$upstream->{'res'}->{'nameservers'}->[0]\"");
+			$response_packet->push(additional => $rr);
+			#$response_packet->print;
+			$socket->send($response_packet->data);
+			$replied = 1;
+			last;
+		}
+	}
+	
+	if(not $replied)
+	{
+		my $response_packet = new Net::DNS::Packet();
+		$response_packet->{'header'}->{'qr'} = 1;
+		$response_packet->{'header'}->{'ra'} = 1;
+		$response_packet->{'header'}->{'rcode'} = 'SERVERR';
+		$response_packet->{'header'}->{'id'} = $incoming_packet->{'header'}->{'id'};;
+		my $rr = Net::DNS::RR->new("upstream-resolver-error. 0 CH TXT \"none of ".(scalar@upstream)." upstreams responded\"");
+		$response_packet->push(additional => $rr);
+		$response_packet->push(question => $_) for $incoming_packet->question;
+		#$response_packet->print;
+		$socket->send($response_packet->data);
+	}
+}
+
+close $socket;
